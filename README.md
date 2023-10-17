ef33164df17a6a98b1b81e11735e4035dbe3be8b
yarwest <yarnoboelens@gmail.com>
2023-10-18 00:07:48 +0200

    Added negative prompt, as well as more parameters and minor fixes

--- README.md
+++ README.md
@@ -22,3 +22,25 @@ After activating the virtual environment, the required dependencies (see `requir
 After succesfully fulfilling the dependencies, the model inference can simply be executed by running the relevant script.
 
 `$ python3 train.py`
+
+## Considerations
+
+Hyperparameters parameters
+
+- Seed: determines starting point of diffusion process. Repeating the process with the same seed will lead to the same output
+- num_inference_steps: number of itterations in diffusion, more itterations leads to a sharper image but will take longer
+- negative_prompts: words to be excluded in diffusion target
+
+By default, stable diffusion produces images of 512 × 512 pixels. It's very easy to override the default using the height and width arguments to create rectangular images in portrait or landscape ratios.
+
+When choosing image sizes, we advise the following:
+
+- Make sure height and width are both multiples of 8.
+- Going below 512 might result in lower quality images.
+- Going over 512 in both directions will repeat image areas (global coherence is lost).
+- The best way to create non-square images is to use 512 in one dimension, and a value larger than that in the other one.
+
+## Sources:
+
+Stable Diffusion with 🧨 Diffusers - Published August 22, 2022: https://huggingface.co/blog/stable_diffusion
+Stable Diffusion 1 vs 2 - What you need to know - Published December 6, 2022: https://www.assemblyai.com/blog/stable-diffusion-1-vs-2-what-you-need-to-know/
--- hf_text2img/train.py
+++ hf_text2img/train.py
@@ -2,15 +2,16 @@ import argparse
 import logging
 import sys
 import os
+from PIL import Image
 
-from diffusers import StableDiffusionPipeline
+from diffusers import (
+    StableDiffusionPipeline,
+    StableDiffusionImg2ImgPipeline,
+)
+
 import torch
 
 __author__ = "Yarno Boelens"
-
-
-np.set_printoptions(suppress=True)
-
 
 # Logging
 logging.basicConfig(
@@ -22,27 +23,64 @@ logging.basicConfig(
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", help="Directory with training data", type=str, required=False)
     parser.add_argument("--prompt", help="Prompt for image generation", type=str, required=True)
+    parser.add_argument("--base_img", help="Base image used for img2img diffusion", type=str, required=False)
+    parser.add_argument("--data_dir", help="Directory with training data", type=str, required=False)
     parser.add_argument("--count", help="Number of generated images", type=str, required=False)
+    parser.add_argument("--seed", help="Seed used for generation of random noise starting image", type=int, required=False, default=512)
+    parser.add_argument("--num_inference_steps", help="The number of inference steps. If you want faster results you can use a smaller number. If you want potentially higher quality results, you can use larger numbers.", type=int, required=False, default=50)
+    parser.add_argument("--img_w", help="Height of generated image", type=int, required=False, default=512)
+    parser.add_argument("--img_h", help="Width of generated image", type=int, required=False, default=768)
+
     return parser.parse_args()
 
 def main(args):
     # Create output directory if not exists
     if not os.path.exists('./out'):
-        os.makedirs('./out/')
+        os.makedirs('out/')
 
     num_images = args.count or 1
     prompt = [args.prompt] * num_images
 
-    pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
-    generator = torch.Generator("cuda").manual_seed(1024)
-    images = pipe(prompt, guidance_scale=7.5, num_inference_steps=50, generator=generator).images
+    negativePrompt = ["ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy"] * num_images
+
+    # init pipelines & components
+    text2img = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4",
+                                                    #revision="fp16", torch_dtype=torch.float32
+            )
+    img2img = StableDiffusionImg2ImgPipeline(**text2img.components)
+    generator = torch.manual_seed(args.seed)
+
+    if args.base_img:
+        base_image = Image.open(args.base_img).convert("RGB")
+        base_image = base_image.resize((768, 512))
+        pipe = img2img(
+            prompt=prompt,
+            negative_prompt=negativePrompt,
+            image=base_image,
+            guidance_scale=7.5,
+            height=args.img_h,
+            width=args.img_w,
+            num_inference_steps=args.num_inference_steps,
+            generator=generator
+        )
+    else:
+        pipe = text2img(
+            prompt,
+            negative_prompt=negativePrompt,
+            guidance_scale=7.5,
+            height=args.img_h,
+            width=args.img_w,
+            num_inference_steps=args.num_inference_steps,
+            generator=generator
+        )
+
+    images = pipe.images
 
     fileName = '_'.join(args.prompt.split(' '))
-    for image, i in images:
+    for i, image in enumerate(images):
         # save image with
-        image.save(f"./out/{fileName}{i}.png")
+        image.save(f"./out/{fileName}-{args.seed}-{args.num_inference_steps}its-{i}.png")
 
 
 if __name__ == "__main__":
