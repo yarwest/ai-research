60f006bc849497f9a1dca11f09ad9d83ad19114d
joankab <120413720+joankab@users.noreply.github.com>
2023-10-17 14:58:39 +0200

    Added count argument to text2img generator

--- hf_text2img/train.py
+++ hf_text2img/train.py
@@ -24,6 +24,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", help="Directory with training data", type=str, required=False)
     parser.add_argument("--prompt", help="Prompt for image generation", type=str, required=True)
+    parser.add_argument("--count", help="Number of generated images", type=str, required=False)
     return parser.parse_args()
 
 def main(args):
@@ -31,12 +32,17 @@ def main(args):
     if not os.path.exists('./out'):
         os.makedirs('./out/')
 
+    num_images = args.count or 1
+    prompt = [args.prompt] * num_images
+
     pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
-    
-    image = pipe(args.prompt).images[0]
+    generator = torch.Generator("cuda").manual_seed(1024)
+    images = pipe(prompt, guidance_scale=7.5, num_inference_steps=50, generator=generator).images
+
     fileName = '_'.join(args.prompt.split(' '))
-    # save image with
-    image.save(f"./out/{fileName}.png")
+    for image, i in images:
+        # save image with
+        image.save(f"./out/{fileName}{i}.png")
 
 
 if __name__ == "__main__":
