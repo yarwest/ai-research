02971b0f8b00b60cddaa2b714788fd4148cf73f9
yarwest <yarnoboelens@gmail.com>
2023-10-25 21:59:38 +0200

    Expanded transcriber to also slice audio into seperate files to use as TTS data set

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -5,7 +5,7 @@ import json
 import os
 import whisper
 import uuid
-import torch
+from pydub import AudioSegment
 
 __author__ = "Yarno Boelens"
 
@@ -23,48 +23,106 @@ def parse_args():
 
     return parser.parse_args()
 
+def initDirs():
+    # Create output directory if not exists
+    if not os.path.exists(f"./{OUT_DIR}"):
+        os.makedirs(f"./{OUT_DIR}")
+    if not os.path.exists(f"./{OUT_DIR}/{RAW_DIR}"):
+        os.makedirs(f"./{OUT_DIR}/{RAW_DIR}")
+    if not os.path.exists(f"./{OUT_DIR}/{DATASET_DIR}"):
+        os.makedirs(f"./{OUT_DIR}/{DATASET_DIR}")
+    if not os.path.exists(f"./{OUT_DIR}/{DATASET_DIR}/{WAVS_DIR}"):
+        os.makedirs(f"./{OUT_DIR}/{DATASET_DIR}/{WAVS_DIR}")
+
 def getFilePath(fileName):
     return os.path.join(os.path.dirname(__file__), fileName)
 
-def new_dict(old_dict):
+def format_segment(old_dict):
     n = old_dict.copy()
-    [n.pop(key) for key in ['seek','tokens','temperature','avg_logprob','no_speech_prob','compression_ratio']]
+    for key in ['seek','tokens','temperature','avg_logprob','no_speech_prob','compression_ratio']:
+        n.pop(key)
     return n
 
+def isCompleteScentence(text):
+    if text.split(" ").__len__() <= 2:
+        return False
+    return text.endswith(".") or text.endswith("?") or text.endswith("!")
+
+def transcribe(file, filePath, model):
+    if f"{file}.txt" in os.listdir(getFilePath(f"{OUT_DIR}/{RAW_DIR}")):
+        logging.info(f"Already transcribed: {file}")
+        with open(getFilePath(f"{OUT_DIR}/{RAW_DIR}/{file}.txt"), 'r') as file:
+            return json.load(file)
+    
+    result = model.transcribe(filePath, fp16=False, language="en")
+    segments = [format_segment(s) for s in result["segments"]]
+    with open(f"./{OUT_DIR}/{RAW_DIR}/{file}.txt", 'a+') as outfile:
+        outfile.write(json.dumps(segments, indent=4))
+    return segments
+
+def sliceSegments(segments, baseAudio):
+    exportedSegments = {}
+    previousSegments = []
+    previousStart = 0
+
+    # segment: { id: int, start: float, end: float, text: string }
+    for segment in segments:
+        text = segment['text'].strip()
+        if isCompleteScentence(text):
+            if previousSegments.__len__() > 0:
+                t1 = previousStart * 1000 #Works in milliseconds
+            else:
+                t1 = segment['start'] * 1000 #Works in milliseconds
+            
+            t2 = segment['end'] * 1000
+            newAudio = baseAudio[t1:t2]
+            audioID = uuid.uuid4()
+            newAudio.export(f"./{OUT_DIR}/{DATASET_DIR}/{WAVS_DIR}/{audioID}.wav", format="wav")
+            previousSegments.append(text)
+            exportedSegments[audioID] = " ".join(previousSegments)
+            previousSegments = []
+            previousStart = 0
+        else:
+            if previousSegments.__len__() == 0:
+                previousStart = segment['start']
+            previousSegments.append(text)
+
+    return exportedSegments
+    
+
+DATA_DIR = 'data'
+OUT_DIR = 'out'
+RAW_DIR = 'raw'
+DATASET_DIR = 'yarnoDataset'
+WAVS_DIR = 'wavs'
+
 def main(args):
-    # Create output directory if not exists
-    if not os.path.exists('./out'):
-        os.makedirs('out/')
-
+    initDirs()
     processID = uuid.uuid4()
-
     print("---- Process ID:", processID)
     logging.info('---- Initialising Whisper AI ----')
-    # Get device
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    # transcribe audio file
-    AUDIO_FILE = os.path.join(os.path.dirname(__file__), f"yarno_voice.wav")
 
     try:
-        # use the audio file as the audio source
         model = whisper.load_model("base")
-        logging.info('---- Transcribing audio ----')
-        for file in os.listdir(getFilePath("data/")):
-            filePath = getFilePath(f"data/{file}")
+        logging.info('---- Starting loop of data dir ----')
+        # Loop through data filder
+        for file in os.listdir(getFilePath(f"{DATA_DIR}/")):
+            filePath = getFilePath(f"{DATA_DIR}/{file}")
             if not os.path.isfile(filePath):
                 continue
-            # TODO: fix (doesnt work rn)
-            if file in os.listdir(getFilePath("out/")):
-                logging.info(f"Already transcribed: {file}")
-            result = model.transcribe(filePath, fp16=False, language="en")
-            logging.info("Transcription: ", result)
-            print(result)
-            for key in result:
-                print(f"{key}")
-            segments = map(new_dict,result["segments"])
-            with open(f'./out/{processID}-{file}.txt', 'a+') as outfile:
-                outfile.write(json.dumps(list(segments), indent=4))
+
+            logging.info('---- Transcribing audio ----')
+            segments = transcribe(file, filePath, model)
+            
+            baseAudio = AudioSegment.from_wav(filePath)
+            
+            logging.info('---- Slicing audio ----')
+            exportedSegments = sliceSegments(segments, baseAudio)
+
+            # store segment data in metadata.txt
+            with open(f"./{OUT_DIR}/{DATASET_DIR}/metadata.txt", 'a+') as outfile:
+                for segID in exportedSegments:
+                    outfile.write(f"{segID}.wav|{exportedSegments[segID]}|{exportedSegments[segID]}\n")
     except Exception as e:
         logging.info(f"---- Failed to transcribe ----", e)
 
