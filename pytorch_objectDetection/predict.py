730b522b19de43ca677df22b58c07d08af9d685f
Yarno Boelens <yarnoboelens@gmail.com>
2023-11-03 17:08:18 +0100

    attempt to implement foreground separation in predict.py

--- pytorch_objectDetection/predict.py
+++ pytorch_objectDetection/predict.py
@@ -51,10 +51,59 @@ def print_annotated_image(
         )
 
     if output_path is not None:
-        cv2.imwrite(output_path, image)
+        printImg(image, output_path)
+
+    extractForeground(image)
 
     # _show_cv2_img("Predicted image", image)
 
+def printImg(image, path):
+    cv2.imwrite(output_path, image)
+
+def extractForeground(image):
+    # create a simple mask image similar 
+    # to the loaded image, with the  
+    # shape and return type 
+    mask = np.zeros(image.shape[:2], np.uint8) 
+   
+    # specify the background and foreground model 
+    # using numpy the array is constructed of 1 row 
+    # and 65 columns, and all array elements are 0 
+    # Data type for the array is np.float64 (default) 
+    backgroundModel = np.zeros((1, 65), np.float64) 
+    foregroundModel = np.zeros((1, 65), np.float64) 
+   
+    # define the Region of Interest (ROI) 
+    # as the coordinates of the rectangle 
+    # where the values are entered as 
+    # (startingPoint_x, startingPoint_y, width, height) 
+    rectangle = (20, 100, 150, 150) 
+   
+    # apply the grabcut algorithm with appropriate 
+    # values as parameters, number of iterations = 3  
+    # cv2.GC_INIT_WITH_RECT is used because 
+    # of the rectangle mode is used  
+    cv2.grabCut(image, mask, rectangle,   
+            backgroundModel, foregroundModel, 
+            3, cv2.GC_INIT_WITH_RECT) 
+   
+    # In the new mask image, pixels will  
+    # be marked with four flags  
+    # four flags denote the background / foreground  
+    # mask is changed, all the 0 and 2 pixels  
+    # are converted to the background 
+    # mask is changed, all the 1 and 3 pixels 
+# are now the part of the foreground 
+    # the return type is also mentioned, 
+    # this gives us the final mask 
+    mask2 = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8') 
+   
+    # The final mask is multiplied with  
+    # the input image to give the segmented image. 
+    image = image * mask2[:, :, np.newaxis]
+    printImg(image, getDatasetPath(f"out/123-foreground.png"))
+    
+      
 
 def _show_cv2_img(window_title, image):
     # Based on https://medium.com/@mh_yip/ee51616f7088
